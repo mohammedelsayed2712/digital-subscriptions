bb3b02f3e8950b41acde5f21bf6ef600bd5e2e39
mohammedelsayed2712 <mohammedelsayed2712@gmail.com>
2025-03-27 17:47:22 +0200

    make stripe command

---
+++ app/Console/Commands/StripePlansCommand.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Console\Commands;
+
+use App\Models\Plan;
+use Illuminate\Console\Command;
+
+class StripePlansCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'stripe:plans';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Stripe plans';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
+        $plans  = $stripe->products->all();
+
+        foreach ($plans->data as $plan) {
+            $price = $stripe->prices->retrieve($plan->default_price, []);
+            Plan::updateOrCreate([
+                'name'            => $plan->name,
+                'slug'            => str_replace(' ', '-', strtolower($plan->name)),
+                'price'           => $price->unit_amount / 100,
+                'interval'        => $price->recurring->interval,
+                'stripe_price_id' => $price->id,
+            ]);
+        }
+
+    }
+}
---
+++ database/migrations/0001_01_01_000002_create_plans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->integer('price');
+            $table->string('stripe_price_id');
+            $table->string('interval');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
