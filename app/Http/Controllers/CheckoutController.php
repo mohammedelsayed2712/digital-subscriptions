e5faeef124da9e107cbacbb0aa795ada342523e8
mohammedelsayed2712 <mohammedelsayed2712@gmail.com>
2025-03-30 18:51:50 +0200

    adds

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Plan;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CheckoutController extends Controller
@@ -17,5 +18,12 @@ class CheckoutController extends Controller
     public function index2(Plan $plan)
     {
         return view('checkout.payment-method', get_defined_vars());
+    }
+
+    public function post(Request $request)
+    {
+        $plan    = Plan::findOrFail($request->plan_id);
+        $payment = Auth::user()->newSubscription($plan->slug, $plan->stripe_price_id)->create($request->payment_method);
+        dd($payment);
     }
 }
